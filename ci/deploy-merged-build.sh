048d6bac3c31a07e3e7e2bf55e14f31e0bbd0a3c
Paweł Wiejkut <poczta@pawelwiejkut.net>
2021-12-28 19:02:09 +0100

    Create deploy-merged-build.sh

---
+++ ci/deploy-merged-build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Build merged
+npm run merge || exit 1
+wc -l ./zbw_adso_bof.abap
+
+# Deploy artifacts
+git clone https://github.com/pawelwiejkut/bw_trfn_tester.git
+cp zbw_adso_bof.abap bw_trfn_tester/last_build/zbw_adso_bof.abap
+cd bw_trfn_tester
+
+# Commit
+git status
+git config user.email "ci@pawelwiejkut.net"
+git config user.name "CI"
+git add last_build/zbw_adso_bof
+
+.abap
+git commit -m "CI build [skip ci]" || exit 1
+git push -q https://$GITHUB_API_KEY@github.com/pawelwiejkut/bw_trfn_tester.git 
